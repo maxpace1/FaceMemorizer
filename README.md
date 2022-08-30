552da20b21e9ca22174fcd824cbb411f55e2e590
Max Pace <maxpace1@icloud.com>
2022-08-29 21:55:53 -0400

    Face memorizer

---
+++ README.md
@@ -0,0 +1,4 @@
+### Face Memorizer
+A program to help you remember people's faces!
+Simply put all the face images you'd like to learn in the `faces` directory and run the script.
+All images must follow the naming format "FirstName_LastName.jpeg".
---
+++ face_memo.py
@@ -0,0 +1,53 @@
+import os
+import random
+import cv2 as cv
+from cv2 import imshow
+
+def quiz_batch(faces):
+  errs = []
+
+  for face in faces:
+    first,last = face.split("_")
+    last = last[:-5]
+    full_name = first + " " + last
+    img = cv.imread(dir + face)
+    imshow("Who is this?", img)
+    print(">>> Who is this? ")
+    guess = ""
+    cv.waitKey(1)
+    guess = input("")
+
+    if guess.casefold() == full_name.casefold():
+      print("Correct!")
+    else:
+      print("Incorrect: expected " + full_name)
+      errs.append(full_name)
+      if guess == "":
+        print("Please type the correct name.")
+        while guess.casefold() != full_name.casefold():
+          guess = input("")
+
+    input(">>> Press return to continue ")
+  return errs
+
+dir = os.path.dirname(os.path.realpath(__file__)) + "/faces/"
+faces = os.listdir(dir)
+faces = list(filter(lambda f: f[0] != ".", faces))
+random.shuffle(faces)
+
+batch_size = int(input(">>> How many faces do you want to learn at a time? "))
+n_batches = (len(faces) + batch_size - 1) // batch_size
+
+for i in range(n_batches):
+  print(f"Round {i}!")
+  batch = faces[i * batch_size : (i+1) * batch_size]
+  while True:
+    errs = quiz_batch(batch)
+    print(f"\nYou got {len(batch)-len(errs)}/{len(batch)} correct!")
+    if len(errs) == 0:
+      print("Congrats!\n")
+      break
+    print("You missed:")
+    for err in errs:
+      print(err)
+    random.shuffle(batch)
